7b37d63674e434d3905138d6f2aca7d513175c4e
Sampad <bebartasampad@gmail.com>
2021-02-13 19:19:05 +0530

    Find Maximum Float Number using Generics

--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -2,7 +2,6 @@ import com.bridgelabz.MaximumNumber;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class MaximumNumberTest {
 
     // Integer Number
@@ -26,4 +25,25 @@ public class MaximumNumberTest {
         int maxNum = maximumNumber.findMaxIntegerValue(100, 200, 400);
         Assert.assertEquals(400, maxNum);
     }
+
+    // Float Number
+    @Test
+    public void givenThreeFloatNumber_findMaximumNumber_shouldReturnSameNo() {
+        MaximumNumber maximumNumber = new MaximumNumber();
+        Float maxFloatValue = maximumNumber.findMaxFloatValue(40.40f, 10.10f, 20.20f);
+        Assert.assertEquals((Float) 40.40f, maxFloatValue);
+    }
+    @Test
+    public void givenThreeFloatNumber_findMaximumNumber_shouldReturnSecondNo() {
+        MaximumNumber maximumNumber = new MaximumNumber();
+        Float maxFloatValue = maximumNumber.findMaxFloatValue(10.10f, 40.40f, 20.20f);
+        Assert.assertEquals((Float) 40.40f, maxFloatValue);
+    }
+    @Test
+    public void givenThreeFloatNumber_findMaximumNumber_shouldReturnThirdNo() {
+        MaximumNumber maximumNumber = new MaximumNumber();
+        Float maxFloatValue = maximumNumber.findMaxFloatValue(10.10f, 20.20f, 40.40f);
+        Assert.assertEquals((Float) 40.40f, maxFloatValue);
+    }
+
 }
